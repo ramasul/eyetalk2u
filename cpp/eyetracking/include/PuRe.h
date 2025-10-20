d78d443d3ac7d94989a03c83b1764b693e994d4c
ramasul <ramasulaimannurcahyo@mail.ugm.ac.id>
2025-10-20 22:06:14 +0700

    feat: (Need Fixing) Pupil Detection

--- cpp/eyetracking/include/PuRe.h
+++ cpp/eyetracking/include/PuRe.h
@@ -17,7 +17,7 @@ namespace pure {
 	struct Confidence{
 		double value = 0;
 		double aspect_ratio = 0;
-		double angular_speed = 0;
+		double angular_spread = 0;
 		double outline_contrast = 0;
 	};
 
@@ -33,4 +33,52 @@ namespace pure {
 
 	typedef std::vector<cv::Point> Segment;
 
+	class Detector {
+	public:
+		Result detect(const cv::Mat& gray_img, cv::Mat* debug_color_img = nullptr);
+		Parameters params;
+
+	private: 
+		// Preprocessing
+		cv::Mat orig_img;
+		cv::Mat debug_img;
+		bool debug = false; // Set to true to enable debug image generation.
+		double scaling_factor;
+		bool preprocess(const cv::Mat& input_img);
+		void postprocess(Result& final_result, const cv::Mat& input_img, cv::Mat* debug_color_img);
+
+	private:
+		//Edge detection and Morphological Manipulation
+		cv::Mat edge_img;
+		void detect_edges();
+
+	private: 
+		// Edge Segment Selection and Confidence Measure
+		std::vector<Segment> segments;
+		std::vector<Result> candidates;
+		double min_pupil_diameter, max_pupil_diameter;
+		void select_edge_segments();
+
+		void evaluate_segment(const Segment& segment, Result& result) const;
+		bool segment_large_enough(const Segment& segment) const;
+		bool segment_diameter_valid(const Segment& segment) const;
+		bool segment_curvature_valid(const Segment& segment) const;
+		bool axes_ratio_is_invalid(double ratio) const;
+		bool fit_ellipse(const Segment& segment, Result& result) const;
+		bool segment_mean_in_ellipse(const Segment& segment, const Result& result) const;
+
+		Confidence calculate_confidence(const Segment& segment, const Result& result) const;
+		double angular_edge_spread(const Segment& segment, const Result& result) const;
+		double ellipse_outline_contrast(const Result& result) const;
+
+	private: 
+		// Conditional Segment Combination
+		void combine_segments();
+		bool proper_intersection(const cv::Rect& r1, const cv::Rect& r2) const;
+		Segment merge_segments(const Segment& seg1, const Segment& seg2) const;
+
+	private:
+		Result select_final_segment();
+
+	};
 }
